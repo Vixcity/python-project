b2f04296598e2147d0442a6acdaaa43aa13c986b
Vixcity <wengxin@hzpnc.com>
2021-01-19 18:25:57 +0800

    获取谷歌浏览记录

---
+++ 爬虫/获取谷歌历史记录.py
@@ -0,0 +1,24 @@
+import sqlite3
+from urllib.parse import urlparse
+import matplotlib.pyplot as plt
+
+websiteDict={}
+
+conn = sqlite3.connect('C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\history')
+cursor=conn.execute("select url from urls")
+
+for item in cursor:
+    website = urlparse(item[0]).netloc
+    count = websiteDict.setdefault(website,0)
+    websiteDict[website] = count + 1
+
+lst_his = sorted(websiteDict.items(),key=lambda x:x[1],reverse=True)
+
+plt.pie(x = [e[1] for e in lst_his[:10]], \
+        labels = [e[0] for e in lst_his[:10]],\
+        autopct='%1.2f%%')
+
+plt.savefig('website.jpg')
+plt.show()
+
+print('程序执行成功！！')
