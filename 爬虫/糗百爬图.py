d150d931ef798a5086f14a08ced72f0e689c2a49
Vixcity <Vixcity>
2021-07-29 16:35:45 +0800

    choubai

--- 爬虫/糗百爬图.py
+++ 爬虫/糗百爬图.py
@@ -6,32 +6,39 @@ import requests
 from bs4 import BeautifulSoup
 import base64
 from io import BytesIO
+import os
+
+if not os.path.exists('choubai/'):
+    os.makedirs('choubai/')
+
 
 def getImg(allHtml):
     # 获取到我们需要的img图片
-    allImg=allHtml.find_all("img",class_='illustration')
-    for index,img in enumerate(allImg):
+    allImg = allHtml.find_all("img", class_='illustration')
+    for index, img in enumerate(allImg):
         # 获取到图片的url
         imgurl = img['src']
-        images = requests.get("https:"+imgurl)
+        images = requests.get("https:" + imgurl)
         Img64 = base64.b64encode(BytesIO(images.content).read())
         imgdata = base64.b64decode(Img64)
         # 这两个代码不知道为啥不行,就用了其他的库
         # 下面两句代码好像转base64编码出错
         # Img64 = base64.b64encode(requests.get("https:"+imgurl).content)
         # print(requests.get("https:"+imgurl).content)
-        with open('D:\\python学习\\day01\\爬虫\\choubai\\'+str(index)+'.jpg', 'wb') as f:
+        with open('choubai/' + str(index) + '.jpg', 'wb') as f:
             f.write(imgdata)
+
 
 def main():
     # UA伪装请求数据
-    url='https://www.qiushibaike.com/imgrank/'
+    url = 'https://www.qiushibaike.com/imgrank/'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
     }
-    allHtml = requests.get(url=url,headers=headers).text
-    soup = BeautifulSoup(allHtml,"html.parser")
+    allHtml = requests.get(url=url, headers=headers).text
+    soup = BeautifulSoup(allHtml, "html.parser")
     getImg(soup)
+
 
 if __name__ == '__main__':
     main()
